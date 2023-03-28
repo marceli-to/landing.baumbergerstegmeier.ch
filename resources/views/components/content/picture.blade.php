495a3969e63f859f47a7c6471b1b012c7685fff3
Marcel Stadelmann <m@marceli.to>
2023-03-28 14:29:49 +0200

    wip

--- resources/views/components/content/picture.blade.php
+++ resources/views/components/content/picture.blade.php
@@ -2,6 +2,6 @@
 <picture>
   <source media="(min-width: 1200px)" srcset="/media/{{ $image }}-lg.jpg">        
   <source media="(min-width: 768px)" srcset="/media/{{ $image }}-md.jpg">
-  <source srcset="/media/{{ $image }}-sm.jpg">
+  <source srcset="/media/{{ $image }}-sm.jpg" loading="lazy">
   <img src="/media/{{ $image }}-sm.jpg" alt="{{ $alt }}" title="{{ $alt }}" height="1600" width="1000" class="w-full h-auto object-cover">
 </picture>
