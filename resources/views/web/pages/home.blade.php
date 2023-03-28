8be98b8504f4a3909566c5d3e300f878b4105a06
Marcel Stadelmann <m@marceli.to>
2023-03-28 16:07:59 +0200

    wip

--- resources/views/web/pages/home.blade.php
+++ resources/views/web/pages/home.blade.php
@@ -13,7 +13,7 @@
   </section>
   <section class="md:flex md:justify-between mt-8 xl:mt-12">
     <div>
-      <h1 class="text-xl xl:text-2xl leading-none mb-10 font-regular">1.4.23: Wir sind umgezogen</h1>
+      <h1 class="text-xl xl:text-2xl leading-none mb-10 xl:mb-7 font-regular">1.4.23: Wir sind umgezogen</h1>
       <p class="mb-0 leading-6">Baumberger Stegmeier Architektur<br>Weststrasse 70, CH-8003 Zürich<br>
         <a href="mailto:info@baumbergerstegmeier.ch">info@baumbergerstegmeier.ch</a>, <a href="tel:+41435005400">+41 43 500 54 00</a>
       </p>
